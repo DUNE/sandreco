708c16c570a5c2e170e290e988bf52ea0652dd09
Matteo Tenti <tenti@bo.infn.it>
2018-06-06 12:16:34 +0200

    script to evaluate radiator mass

---
+++ radmass.cpp
@@ -0,0 +1,29 @@
+double rmass = 0.; // kg
+
+void searchRad(TGeoVolume* vol)
+{
+  TString name(vol->GetName());
+  
+  if(name.Contains("Radiator"))
+  {
+    rmass += vol->Weight();
+    std::cout << "Node: " << vol->GetName() << std::endl;
+  } 
+  
+  for(int i = 0; i < vol->GetNdaughters(); i++)
+  {
+    searchRad(vol->GetNode(i)->GetVolume());
+  }
+}
+
+void radmass()
+{
+  TGeoManager* geo = TGeoManager::Import("../../../wd/config/geo/files/geometry_v11.gdml");
+  
+  rmass = 0.;
+  
+  searchRad(geo->GetTopVolume());
+  
+  std::cout << "Total radiator mass: " << rmass << " kg" << std::endl;
+  
+}
