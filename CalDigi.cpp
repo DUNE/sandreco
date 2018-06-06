e9dc8465042d7d393b729686fdba30fa57953f53
Matteo Tenti <tenti@bo.infn.it>
2018-06-06 14:39:34 +0200

    Added histograms for timing per layers and PE per layers

--- CalDigi.cpp
+++ CalDigi.cpp
@@ -10,6 +10,7 @@
 #include <TH1I.h>
 #include <TH2D.h>
 #include <TRandom3.h>
+#include <TStyle.h>
 
 #include "/mnt/nas01/users/mtenti/sw/edep-sim/edep-sim-bin/include/edep-sim/TG4Event.h"
 #include "/mnt/nas01/users/mtenti/sw/edep-sim/edep-sim-bin/include/edep-sim/TG4HitSegment.h"
@@ -370,11 +371,26 @@ void checkfast(const char* fname)
   
   TH1I* h_id  = new TH1I("h_id","Cell ID",50000,-25000,25000);
   TH1I* h_pe  = new TH1I("h_pe","pe",100,0,100);
+  
   TH1I* h_adc = new TH1I("h_adc","adc",100,0,100);
   TH1D* h_tdc = new TH1D("h_tdc","tdc",200,0,100);
-  TH1D* h_tdif = new TH1D("h_tdif","diff",500,-100,100);
-  TH1D* h_tsum = new TH1D("h_tsum","sum",200,0,100);
+  
+  TH1D* h_tdif = new TH1D("h_tdif","diff",100,-20,20);
+  TH1D* h_tsum = new TH1D("h_tsum","sum",100,20,40);
+  
   TH2D* h1 = new TH2D("h1","",100,0,100,100,0,100);
+  
+  TH1D* hlaysum1 = new TH1D("hlaysum1","",100,20,35);
+  TH1D* hlaysum2 = new TH1D("hlaysum2","",100,20,35);
+  TH1D* hlaysum3 = new TH1D("hlaysum3","",100,20,35);
+  TH1D* hlaysum4 = new TH1D("hlaysum4","",100,20,35);
+  TH1D* hlaysum5 = new TH1D("hlaysum5","",100,20,35);
+  
+  TH1D* hlaysumADC1 = new TH1D("hlaysumADC1","",100,0,200);
+  TH1D* hlaysumADC2 = new TH1D("hlaysumADC2","",100,0,200);
+  TH1D* hlaysumADC3 = new TH1D("hlaysumADC3","",100,0,200);
+  TH1D* hlaysumADC4 = new TH1D("hlaysumADC4","",100,0,200);
+  TH1D* hlaysumADC5 = new TH1D("hlaysumADC5","",100,0,200);
   
   h_id ->SetDirectory(0);
   h_pe ->SetDirectory(0);
@@ -384,6 +400,18 @@ void checkfast(const char* fname)
   h_tdif->SetDirectory(0);
   h_tsum->SetDirectory(0);
   h1->SetDirectory(0);
+  
+  hlaysum1->SetDirectory(0);
+  hlaysum2->SetDirectory(0);
+  hlaysum3->SetDirectory(0);
+  hlaysum4->SetDirectory(0);
+  hlaysum5->SetDirectory(0);
+  
+  hlaysumADC1->SetDirectory(0);
+  hlaysumADC2->SetDirectory(0);
+  hlaysumADC3->SetDirectory(0);
+  hlaysumADC4->SetDirectory(0);
+  hlaysumADC5->SetDirectory(0);
   
   //const int nev = t->GetEntries();
   const int nev = 1000;
@@ -403,11 +431,47 @@ void checkfast(const char* fname)
       h_pe->Fill(it->second.size());
     }
     
+    double ADClay1 = 0;
+    double ADClay2 = 0;
+    double ADClay3 = 0;
+    double ADClay4 = 0;
+    double ADClay5 = 0;
+    
     for(std::map<int, double>::iterator it=adc->begin(); it != adc->end(); ++it)
     {
       h_adc->Fill(it->second);
       h1->Fill(it->second,(*tdc)[it->first]);
+      
+      int id_lay = TMath::Abs(int(it->first/100)%10);
+          
+      switch(id_lay)
+      {
+         case 0:
+           ADClay1 += it->second;
+         break;
+         case 1:
+           ADClay2 += it->second;
+         break;
+         case 2:
+           ADClay3 += it->second;
+         break;
+         case 3:
+           ADClay4 += it->second;
+         break;
+         case 4:
+           ADClay5 += it->second;
+         break;
+         default:
+           std::cout << "error: layer id: " << id_lay << std::endl;
+         break; 
+      }
     }
+    
+    hlaysumADC1->Fill(ADClay1);
+    hlaysumADC2->Fill(ADClay2);
+    hlaysumADC3->Fill(ADClay3);
+    hlaysumADC4->Fill(ADClay4);
+    hlaysumADC5->Fill(ADClay5);
     
     for(std::map<int, double>::iterator it=tdc->begin(); it != tdc->end(); ++it)
     {
@@ -418,6 +482,36 @@ void checkfast(const char* fname)
         {
           h_tsum->Fill(it->second + (*tdc)[-1*it->first]);
           h_tdif->Fill(it->second - (*tdc)[-1*it->first]);
+        }
+      }
+      
+      if(it->first > 0)
+      {
+        if(it->second > 0 && (*tdc)[-1*it->first] > 0)
+        {
+          int id_lay = int(it->first/100)%10;
+          
+          switch(id_lay)
+          {
+             case 0:
+               hlaysum1->Fill(it->second + (*tdc)[-1*it->first]);
+             break;
+             case 1:
+               hlaysum2->Fill(it->second + (*tdc)[-1*it->first]);
+             break;
+             case 2:
+               hlaysum3->Fill(it->second + (*tdc)[-1*it->first]);
+             break;
+             case 3:
+               hlaysum4->Fill(it->second + (*tdc)[-1*it->first]);
+             break;
+             case 4:
+               hlaysum5->Fill(it->second + (*tdc)[-1*it->first]);
+             break;
+             default:
+               std::cout << "error: layer id: " << id_lay << std::endl;
+             break; 
+          }
         }
       }
     }
@@ -434,9 +528,38 @@ void checkfast(const char* fname)
   TCanvas* c4 = new TCanvas();
   h_tdc->Draw();
   TCanvas* c5 = new TCanvas();
-  h_tsum->Draw();
+  h_tsum->Fit("gaus");
+  h_tsum->Draw("E0");
   TCanvas* c6 = new TCanvas();
-  h_tdif->Draw();
+  h_tdif->Fit("gaus");
+  h_tdif->Draw("E0");
   TCanvas* c7 = new TCanvas();
   h1->Draw("colz");
+  gStyle->SetOptFit(1);
+  TCanvas* c8 = new TCanvas();
+  hlaysum1->Fit("gaus");
+  hlaysum1->Draw("E0");
+  TCanvas* c9 = new TCanvas();
+  hlaysum2->Fit("gaus");
+  hlaysum2->Draw("E0");
+  TCanvas* c10 = new TCanvas();
+  hlaysum3->Fit("gaus");
+  hlaysum3->Draw("E0");
+  TCanvas* c11 = new TCanvas();
+  hlaysum4->Fit("gaus");
+  hlaysum4->Draw("E0");
+  TCanvas* c12 = new TCanvas();
+  hlaysum5->Fit("gaus");
+  hlaysum5->Draw("E0");
+  
+  TCanvas* c13 = new TCanvas();
+  hlaysumADC1->Draw("E0");
+  TCanvas* c14 = new TCanvas();
+  hlaysumADC2->Draw("E0");
+  TCanvas* c15 = new TCanvas();
+  hlaysumADC3->Draw("E0");
+  TCanvas* c16 = new TCanvas();
+  hlaysumADC4->Draw("E0");
+  TCanvas* c17 = new TCanvas();
+  hlaysumADC5->Draw("E0");
 }
