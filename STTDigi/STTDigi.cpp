86bb5292a115d96233fda8fecfacc1d0bc583696
Matteo Tenti <tenti@bo.infn.it>
2018-07-16 00:36:26 +0200

    hit filter moved in reconstruction (before was in digitization)

--- STTDigi/STTDigi.cpp
+++ STTDigi/STTDigi.cpp
@@ -20,80 +20,6 @@ void STTDigi()
   std::cout << "finname could contain wild card" << std::endl;
 }
 
-double mindist(double s1x, double s1y, double s1z,
-               double s2x, double s2y, double s2z,
-               double px, double py, double pz)
-{
-    double segmod = (s1x - s2x)*(s1x - s2x)
-                    +(s1y - s2y)*(s1y - s2y)
-                    +(s1z - s2z)*(s1z - s2z);
-
-    double prod = (px - s1x)*(s2x - s1x)
-                  +(py - s1y)*(s2y - s1y)
-                  +(pz - s1z)*(s2z - s1z);
-
-    double t = std::min(std::max(prod/segmod,0.),1.);
-
-    double s3x = s1x + (s2x - s1x) * t;
-    double s3y = s1y + (s2y - s1y) * t;
-    double s3z = s1z + (s2z - s1z) * t;
-    
-    return sqrt((px - s3x)*(px - s3x)
-                +(py - s3y)*(py - s3y)
-                +(pz - s3z)*(pz - s3z));
-}
-
-double angle(double x1, double y1, double z1,
-             double x2, double y2, double z2)
-{
-    double prod = x1*x2+y1*y2+z1*z2;
-    double mag1 = sqrt(x1*x1+y1*y1+z1*z1);
-    double mag2 = sqrt(x2*x2+y2*y2+z2*z2);
-
-    return TMath::ACos(prod/(mag1*mag2));
-}
-
-bool ishitok(TG4Event* ev, TG4HitSegment hit,
-             double postol = 5., double angtol = 0.3)
-{
-    double x = 0.5*(hit.Start.X()+hit.Stop.X());
-    double y = 0.5*(hit.Start.Y()+hit.Stop.Y());
-    double z = 0.5*(hit.Start.Z()+hit.Stop.Z());
-
-    std::vector<double> dpos;
-    std::vector<double> dang;
-    for(unsigned int jj = 0; jj < ev->Trajectories.size(); jj++)
-    {
-        if(ev->Trajectories[jj].TrackId == hit.PrimaryId)
-        {
-            for(unsigned int kk = 0; kk < ev->Trajectories[jj].Points.size()-1; kk++)
-            {
-                dpos.push_back(mindist(ev->Trajectories[jj].Points[kk].Position.X(),
-                                       ev->Trajectories[jj].Points[kk].Position.Y(),
-                                       ev->Trajectories[jj].Points[kk].Position.Z(),
-                                       ev->Trajectories[jj].Points[kk+1].Position.X(),
-                                       ev->Trajectories[jj].Points[kk+1].Position.Y(),
-                                       ev->Trajectories[jj].Points[kk+1].Position.Z(),
-                                       x,y,z));
-                dang.push_back(angle(ev->Trajectories[jj].Points[kk+1].Position.X()-ev->Trajectories[jj].Points[kk].Position.X(),
-                                     ev->Trajectories[jj].Points[kk+1].Position.Y()-ev->Trajectories[jj].Points[kk].Position.Y(),
-                                     ev->Trajectories[jj].Points[kk+1].Position.Z()-ev->Trajectories[jj].Points[kk].Position.Z(),
-                                     hit.Stop.X()-hit.Start.X(),
-                                     hit.Stop.Y()-hit.Start.Y(),
-                                     hit.Stop.Z()-hit.Start.Z()));
-            }
-        }
-    }
-    int index = std::distance(dpos.begin(), std::min_element(dpos.begin(),dpos.end()));
-
-    //std::cout << dpos[index] << " " << dang[index] << std::endl;
-
-    if(dpos[index] > postol || dang[index] > angtol)
-        return false;
-    else
-        return true;
-}
-
 void DigitizeSTT(const char* finname, const char* foutname)
 {
   
@@ -113,7 +39,7 @@ void DigitizeSTT(const char* finname, const char* foutname)
   TTree tstt("tSttDigi","KLOE Stt Digitization");
   tstt.Branch("Stt","std::vector<digit>",&digit_vec);
     
-  const int nev = t->GetEntries();
+  const int nev = 2/*t->GetEntries()*/;
   
   std::cout << "Events: " << nev << " [";
   std::cout << std::setw(3) << int(0) << "%]" << std::flush;
@@ -130,9 +56,6 @@ void DigitizeSTT(const char* finname, const char* foutname)
     for(unsigned int j = 0; j < ev->SegmentDetectors["StrawTracker"].size(); j++)
     {
       const TG4HitSegment& hseg = ev->SegmentDetectors["StrawTracker"].at(j);
-      
-      if(!ishitok(ev, hseg))
-        continue;
       
       double x = 0.5 * (hseg.Start.X() + hseg.Stop.X());
       double y = 0.5 * (hseg.Start.Y() + hseg.Stop.Y());
--- reco/Reco.cpp
+++ reco/Reco.cpp
@@ -9,6 +9,91 @@
 #include <iostream>
 #include <algorithm>
 
+double mindist(double s1x, double s1y, double s1z,
+               double s2x, double s2y, double s2z,
+               double px, double py, double pz)
+{
+    double segmod = (s1x - s2x)*(s1x - s2x)
+                    +(s1y - s2y)*(s1y - s2y)
+                    +(s1z - s2z)*(s1z - s2z);
+
+    double prod = (px - s1x)*(s2x - s1x)
+                  +(py - s1y)*(s2y - s1y)
+                  +(pz - s1z)*(s2z - s1z);
+
+    double t = std::min(std::max(prod/segmod,0.),1.);
+
+    double s3x = s1x + (s2x - s1x) * t;
+    double s3y = s1y + (s2y - s1y) * t;
+    double s3z = s1z + (s2z - s1z) * t;
+    
+    return sqrt((px - s3x)*(px - s3x)
+                +(py - s3y)*(py - s3y)
+                +(pz - s3z)*(pz - s3z));
+}
+
+double angle(double x1, double y1, double z1,
+             double x2, double y2, double z2)
+{
+    double prod = x1*x2+y1*y2+z1*z2;
+    double mag1 = sqrt(x1*x1+y1*y1+z1*z1);
+    double mag2 = sqrt(x2*x2+y2*y2+z2*z2);
+
+    return TMath::ACos(prod/(mag1*mag2));
+}
+
+bool ishitok(TG4Event* ev, int trackid, TG4HitSegment hit,
+             double postol = 5., double angtol = 0.3)
+{
+    double x = 0.5*(hit.Start.X()+hit.Stop.X());
+    double y = 0.5*(hit.Start.Y()+hit.Stop.Y());
+    double z = 0.5*(hit.Start.Z()+hit.Stop.Z());
+
+    std::vector<double> dpos;
+    std::vector<double> dang;
+    
+    for(unsigned int jj = 0; jj < ev->Trajectories.size(); jj++)
+    {
+        if(ev->Trajectories[jj].TrackId == trackid)
+        {
+            for(unsigned int kk = 0; kk < ev->Trajectories[jj].Points.size()-1; kk++)
+            {
+                dpos.push_back(mindist(ev->Trajectories[jj].Points[kk].Position.X(),
+                                       ev->Trajectories[jj].Points[kk].Position.Y(),
+                                       ev->Trajectories[jj].Points[kk].Position.Z(),
+                                       ev->Trajectories[jj].Points[kk+1].Position.X(),
+                                       ev->Trajectories[jj].Points[kk+1].Position.Y(),
+                                       ev->Trajectories[jj].Points[kk+1].Position.Z(),
+                                       x,y,z));
+                dang.push_back(angle(ev->Trajectories[jj].Points[kk+1].Position.X()-ev->Trajectories[jj].Points[kk].Position.X(),
+                                     ev->Trajectories[jj].Points[kk+1].Position.Y()-ev->Trajectories[jj].Points[kk].Position.Y(),
+                                     ev->Trajectories[jj].Points[kk+1].Position.Z()-ev->Trajectories[jj].Points[kk].Position.Z(),
+                                     hit.Stop.X()-hit.Start.X(),
+                                     hit.Stop.Y()-hit.Start.Y(),
+                                     hit.Stop.Z()-hit.Start.Z()));
+                /*
+                std::cout << ev->Trajectories[jj].Points[kk].Position.X() << " " <<
+                             ev->Trajectories[jj].Points[kk].Position.Y() << " " <<
+                             ev->Trajectories[jj].Points[kk].Position.Z() << " " <<
+                             ev->Trajectories[jj].Points[kk+1].Position.X() << " " <<
+                             ev->Trajectories[jj].Points[kk+1].Position.Y() << " " <<
+                             ev->Trajectories[jj].Points[kk+1].Position.Z() << " " <<
+                             x << " " << y << " " << z << " " << dpos.back() << " " <<
+                             ev->Trajectories[jj].TrackId << " " << ev->Trajectories[jj].PDGCode << std::endl;
+                */
+            }
+        }
+    }
+    int index = std::distance(dpos.begin(), std::min_element(dpos.begin(),dpos.end()));
+
+    //std::cout << dpos[index] << " " << dang[index] << std::endl;
+
+    if(dpos[index] > postol || dang[index] > angtol)
+        return false;
+    else
+        return true;
+}
+
 int fitCircle(int n, const std::vector<double>& x, const std::vector<double>& y, double &xc, double &yc, double &r, double &errr, double &chi2)
 {
     xc = -999;
@@ -178,7 +263,7 @@ void RecoSTTTrack(const char* fSttDigi, const char* fTrueMC, const char* fOut)
   TTree ttr("tTrack","Track");
   ttr.Branch("track","std::vector<track>",&vec_tr);
     
-  const int nev = t->GetEntries();
+  const int nev = 2/*t->GetEntries()*/;
   
   std::cout << "Events: " << nev << " [";
   std::cout << std::setw(3) << int(0) << "%]" << std::flush;
@@ -215,7 +300,7 @@ void RecoSTTTrack(const char* fSttDigi, const char* fTrueMC, const char* fOut)
       {
         for(unsigned int m = 0; m < digit_vec->at(k).hindex.size(); m++)
         {
-          if(ev->SegmentDetectors["StrawTracker"].at(digit_vec->at(k).hindex.at(m)).PrimaryId == tr.tid)
+          if(ishitok(ev, tr.tid, ev->SegmentDetectors["StrawTracker"].at(digit_vec->at(k).hindex.at(m))))
           {
             tr.digits.push_back(digit_vec->at(k));
             break;
