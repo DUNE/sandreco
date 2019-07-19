fd2e371a48a44286e19d986ca481b7ef342b4ffb
Matteo Tenti <matteo.tenti@bo.infn.it>
2019-07-19 14:17:51 +0200

    small change of env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+DIR=$(dirname ${BASH_SOURCE[0]})
+
 if [[ -z "${ROOT_INCLUDE_PATH}" ]]; then
-  export ROOT_INCLUDE_PATH=${BASH_SOURCE[0]}/../struct
+  export ROOT_INCLUDE_PATH=${DIR}/../struct
 else
-  export ROOT_INCLUDE_PATH=${ROOT_INCLUDE_PATH}:${BASH_SOURCE[0]}/../struct
+  export ROOT_INCLUDE_PATH=${ROOT_INCLUDE_PATH}:${DIR}/../struct
 fi
 
-export PATH=${PATH}:${BASH_SOURCE[0]}/../bin
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${BASH_SOURCE[0]}/../struct
+export PATH=${PATH}:${DIR}/../bin
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${DIR}/../struct
