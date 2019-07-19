968821556f3f00421b5c5330da0cd66d755a105e
Matteo Tenti <matteo.tenti@bo.infn.it>
2019-07-19 14:13:43 +0200

    small change of env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [[ -z "${ROOT_INCLUDE_PATH}" ]]; then
-  export ROOT_INCLUDE_PATH=/wd/dune-it/enurec/analysis/kloe-simu/struct
+  export ROOT_INCLUDE_PATH=${BASH_SOURCE[0]}/../struct
 else
-  export ROOT_INCLUDE_PATH=${ROOT_INCLUDE_PATH}:/wd/dune-it/enurec/analysis/kloe-simu/struct
+  export ROOT_INCLUDE_PATH=${ROOT_INCLUDE_PATH}:${BASH_SOURCE[0]}/../struct
 fi
 
-export PATH=${PATH}:/wd/dune-it/enurec/analysis/kloe-simu/bin
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/wd/dune-it/enurec/analysis/kloe-simu/struct
+export PATH=${PATH}:${BASH_SOURCE[0]}/../bin
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${BASH_SOURCE[0]}/../struct
