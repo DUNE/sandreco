b1a155018b6e506fe7fec3dfa56fddfdbae9142d
Matteo Tenti <matteo.tenti@bo.infn.it>
2019-08-09 16:34:48 +0200

    test of genfit simple program with 7 track (5 mu + pi + p) taken from edep-sim simulation

---
+++ myMinimalFit/env.sh
@@ -0,0 +1,8 @@
+source /wd/dune-it/enurec/analysis/kloe-simu/scripts/env.sh
+source /wd/sw/env.sh
+
+export ROOT_INCLUDE_PATH=${ROOT_INCLUDE_PATH}:/wd/sw/GENFIT/GenFit.binary/include/
+export ROOT_INCLUDE_PATH=${ROOT_INCLUDE_PATH}:/usr/include/eigen3
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/wd/sw/CLHEP/2.4.1.2/binary/lib/
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/wd/sw/GENFIT/GenFit.binary/lib64
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/wd/sw/RAVE/rave-0.6.25.binary/lib
