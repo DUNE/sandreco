6a31603dfa88bc2e29bb3f827419a338c45a567d
Matteo Tenti <matteo.tenti@bo.infn.it>
2020-04-15 20:22:50 +0200

    wip

--- src/fastcheck.cpp
+++ src/fastcheck.cpp
@@ -62,10 +62,12 @@ int main(int argc, char* argv[])
   TCut PrimaryPart = "particles.primary==1";
   TCut PartTrackRecoOK = "particles.tr.ret_ln==0&&particles.tr.ret_cr==0";
   
+  std::cout << "tDigit histograms..." << std::flush;
+  
   if(tDigit)
   {  
-    print(c, fout, tDigit, "cell.mod", "", "", "; module id", -1);
-    print(c, fout, tDigit, "cell.lay", "", "", "; layer id");
+    print(c, fout, tDigit, "cell.mod", "", "", "cells; module id", -1);
+    print(c, fout, tDigit, "cell.lay", "", "", "cells; layer id");
     print(c, fout, tDigit, "cell.cel", barrel_module, "", "barrel modules; cell id");
     print(c, fout, tDigit, "cell.l", barrel_module, "", "barrel modules; cell length (mm)");
     print(c, fout, tDigit, "cell.cel", endcap_module, "", "endcap modules; cell id");
@@ -73,13 +75,13 @@ int main(int argc, char* argv[])
     
     n = tDigit->Draw("cell.y:cell.x", "", "goff"); 
     g = new TGraph(n,tDigit->GetV2(),tDigit->GetV1()); 
-    g->SetTitle("; x cell position (mm); y cell position (mm)");
+    g->SetTitle("cells; x cell position (mm); y cell position (mm)");
     g->Draw("ap");
     c.SaveAs(fout.Data());
     
     n = tDigit->Draw("cell.y:cell.z", "", "goff"); 
     g = new TGraph(n,tDigit->GetV2(),tDigit->GetV1());
-    g->SetTitle("; z cell position (mm); y cell position (mm)");
+    g->SetTitle("cells; z cell position (mm); y cell position (mm)");
     g->Draw("ap");
     c.SaveAs(fout.Data());
     
@@ -91,7 +93,7 @@ int main(int argc, char* argv[])
     
     n = tDigit->Draw("TMath::ATan2(23910.000-cell.z,cell.y+2384.7300)/TMath::Pi()*180.:cell.mod", barrel_module, "goff"); 
     g = new TGraph(n,tDigit->GetV2(),tDigit->GetV1());
-    g->SetTitle("; module id; angle from y axis (rad)");
+    g->SetTitle("barrel modules; module id; angle from y axis (rad)");
     g->Draw("ap");
     c.SaveAs(fout.Data());
     
@@ -115,107 +117,133 @@ int main(int argc, char* argv[])
     
     n = tDigit->Draw("cell.x:cell.mod", endcap_module, "goff"); 
     g = new TGraph(n,tDigit->GetV2(),tDigit->GetV1());
-    g->SetTitle("; module id; x cell position (mm)");
+    g->SetTitle("endcap module; module id; x cell position (mm)");
     g->Draw("ap");
     c.SaveAs(fout.Data());
     
     c.SetLogy(true);
-    print(c, fout, tDigit, "TMath::Log10(cell.adc1)", "", "", "; log_{10}(adc1)");
-    print(c, fout, tDigit, "TMath::Log10(cell.adc2)", "", "", "; log_{10}(adc2)");
-    print(c, fout, tDigit, "TMath::Log10(cell.tdc1)", "", "", "; log_{10}(tdc1/ns)");
-    print(c, fout, tDigit, "TMath::Log10(cell.tdc2)", "", "", "; log_{10}(tdc2/ns)");
-    print(c, fout, tDigit, "TMath::Log10(cell.pe_time1)", "", "", "; log_{10}(p.e. time1/ns)");
-    print(c, fout, tDigit, "TMath::Log10(cell.pe_time2)", "", "", "; log_{10}(p.e. time2/ns)");
+    print(c, fout, tDigit, "TMath::Log10(cell.adc1)", "", "", "cells; log_{10}(adc1)");
+    print(c, fout, tDigit, "TMath::Log10(cell.adc2)", "", "", "cells; log_{10}(adc2)");
+    print(c, fout, tDigit, "TMath::Log10(cell.tdc1)", "", "", "cells; log_{10}(tdc1/ns)");
+    print(c, fout, tDigit, "TMath::Log10(cell.tdc2)", "", "", "cells; log_{10}(tdc2/ns)");
+    print(c, fout, tDigit, "TMath::Log10(cell.pe_time1)", "", "", "cells; log_{10}(p.e. time1/ns)");
+    print(c, fout, tDigit, "TMath::Log10(cell.pe_time2)", "", "", "cells; log_{10}(p.e. time2/ns)");
     c.SetLogy(false);
     
-    n = tDigit->Draw("Stt.y:Stt.x", "", "goff"); 
+    n = tDigit->Draw("Stt.y:Stt.x", "", "goff",1000); 
     g = new TGraph(n,tDigit->GetV2(),tDigit->GetV1()); 
-    g->SetTitle("; x stt digit position (mm); y stt digit position (mm)");
+    g->SetTitle("stt; x stt digit position (mm); y stt digit position (mm)");
     g->Draw("ap");
     c.SaveAs(fout.Data());
     
-    n = tDigit->Draw("Stt.y:Stt.z", "", "goff"); 
+    n = tDigit->Draw("Stt.y:Stt.z", "", "goff",1000); 
     g = new TGraph(n,tDigit->GetV2(),tDigit->GetV1()); 
-    g->SetTitle("; z stt digit position (mm); y stt digit position (mm)");
+    g->SetTitle("stt; z stt digit position (mm); y stt digit position (mm)");
     g->Draw("ap");
     c.SaveAs(fout.Data());
     
     c.SetLogy(true);
-    print(c, fout, tDigit, "TMath::Log10(Stt.t)", "", "", "; log_{10}(stt time/ns)");
-    print(c, fout, tDigit, "TMath::Log10(Stt.de)", "", "", "; log_{10}(dE/MeV)");
+    print(c, fout, tDigit, "TMath::Log10(Stt.t)", "", "", "stt; log_{10}(stt time/ns)");
+    print(c, fout, tDigit, "TMath::Log10(Stt.de)", "", "", "stt; log_{10}(dE/MeV)");
     c.SetLogy(false);
-    print(c, fout, tDigit, "Stt.hor", "", "", "; hor/ver");
+    print(c, fout, tDigit, "Stt.hor", "", "", "stt; hor/ver");
   }
+  
+  std::cout << " done" << std::endl;
+  
+  std::cout << "tReco  histograms..." << std::flush;
   
   if(tReco)
   {
     tReco->Draw("track.ret_ln:track.ret_cr", "", "colz"); 
     h2 = (TH2D*) gROOT->FindObject("htemp");
     h2->SetStats(false);
-    h2->SetTitle(";ret_cr;ret_ln");
+    h2->SetTitle("tracks;ret_cr;ret_ln");
     h2->Draw("colztext");
     c.SaveAs(fout.Data());
     
     c.SetLogy(true);
-    print(c, fout, tReco, "TMath::Log10(track.r)", trackRecoOK, "", "; log_{10}(R/mm)");
-    print(c, fout, tReco, "track.a", trackRecoOK, "", "; a (mm)");
-    print(c, fout, tReco, "track.b", trackRecoOK, "", "; b");
+    print(c, fout, tReco, "TMath::Log10(track.r)", trackRecoOK, "", "tracks; log_{10}(R/mm)");
     c.SetLogy(false);
-    print(c, fout, tReco, "track.h", trackRecoOK, "", "; h");
+    
+    n = tReco->Draw("track.yc:track.zc", trackRecoOK, "goff"); 
+    g = new TGraph(n,tReco->GetV2(),tReco->GetV1()); 
+    g->SetTitle("tracks; zc (mm); yc (mm)");
+    g->Draw("ap");
+    c.SaveAs(fout.Data());
+    
+    c.SetLogy(true);
+    print(c, fout, tReco, "track.a", trackRecoOK, "", "tracks; a (mm)");
+    print(c, fout, tReco, "track.b", trackRecoOK, "", "tracks; b");
+    c.SetLogy(false);
+    print(c, fout, tReco, "track.h", trackRecoOK, "", "tracks; h");
     
     n = tReco->Draw("track.y0:track.x0", trackRecoOK, "goff"); 
     g = new TGraph(n,tReco->GetV2(),tReco->GetV1()); 
-    g->SetTitle("; x0 (mm); y0 (mm)");
+    g->SetTitle("tracks; x0 (mm); y0 (mm)");
     g->Draw("ap");
     c.SaveAs(fout.Data());
     
     n = tReco->Draw("track.y0:track.z0", trackRecoOK, "goff"); 
     g = new TGraph(n,tReco->GetV2(),tReco->GetV1()); 
-    g->SetTitle("; z0 (mm); y0 (mm)");
+    g->SetTitle("tracks; z0 (mm); y0 (mm)");
     g->Draw("ap");
     c.SaveAs(fout.Data());
     
-    print(c, fout, tReco, "track.t0", trackRecoOK, "", "; t (ns)");
-    print(c, fout, tReco, "TMath::Log10(track.chi2_cr)", trackRecoOK, "", "; log_{10}(#chi^{2}_{cr})");
-    print(c, fout, tReco, "TMath::Log10(track.chi2_ln)", trackRecoOK, "", "; log_{10}(#chi^{2}_{ln})");
+    print(c, fout, tReco, "track.t0", trackRecoOK, "", "tracks; t (ns)");
+    print(c, fout, tReco, "TMath::Log10(track.chi2_cr)", trackRecoOK, "", "tracks; log_{10}(#chi^{2}_{cr})");
+    print(c, fout, tReco, "TMath::Log10(track.chi2_ln)", trackRecoOK, "", "tracks; log_{10}(#chi^{2}_{ln})");
+    
+    tReco->Draw("(TMath::Log10(track.chi2_ln)):(TMath::Log10(track.chi2_cr))", trackRecoOK, "colz"); 
+    h2 = (TH2D*) gROOT->FindObject("htemp");
+    h2->SetStats(false);
+    h2->SetTitle("tracks; log_{10}(#chi^{2}_{cr}); log_{10}(#chi^{2}_{ln})");
+    h2->Draw("colz");
+    c.SaveAs(fout.Data());
     
     n = tReco->Draw("cluster.y:cluster.x", "", "goff"); 
     g = new TGraph(n,tReco->GetV2(),tReco->GetV1()); 
-    g->SetTitle("; x (mm); y (mm)");
+    g->SetTitle("clusters; x (mm); y (mm)");
     g->Draw("ap");
     c.SaveAs(fout.Data());
     
     n = tReco->Draw("cluster.y:cluster.z", "", "goff"); 
     g = new TGraph(n,tReco->GetV2(),tReco->GetV1()); 
-    g->SetTitle("; z (mm); y (mm)");
+    g->SetTitle("clusters; z (mm); y (mm)");
     g->Draw("ap");
     c.SaveAs(fout.Data());
     
-    print(c, fout, tReco, "TMath::Log10(cluster.t)", "", "", "; log_{10}(t/ns)");
-    print(c, fout, tReco, "TMath::Log10(cluster.e)", "", "", "; log_{10}(E/MeV)");
-    print(c, fout, tReco, "cluster.sx", "", "", "; sx");
-    print(c, fout, tReco, "cluster.sy", "", "", "; sy");
-    print(c, fout, tReco, "cluster.sz", "", "", "; sz");
-    print(c, fout, tReco, "cluster.varx", "", "", "; varx");
-    print(c, fout, tReco, "cluster.vary", "", "", "; vary");
-    print(c, fout, tReco, "cluster.varz", "", "", "; varz");
+    print(c, fout, tReco, "TMath::Log10(cluster.t)", "", "", "clusters; log_{10}(t/ns)");
+    print(c, fout, tReco, "TMath::Log10(cluster.e)", "", "", "clusters; log_{10}(E/MeV)");
+    print(c, fout, tReco, "cluster.sx", "", "", "clusters; sx");
+    print(c, fout, tReco, "cluster.sy", "", "", "clusters; sy");
+    print(c, fout, tReco, "cluster.sz", "", "", "clusters; sz");
+    c.SetLogy(true);
+    print(c, fout, tReco, "cluster.varx", "", "", "clusters; varx");
+    print(c, fout, tReco, "cluster.vary", "", "", "clusters; vary");
+    print(c, fout, tReco, "cluster.varz", "", "", "clusters; varz");
+    c.SetLogy(false);
   }
+  
+  std::cout << " done" << std::endl;
+  
+  std::cout << "tEvent histograms..." << std::flush;
   
   if(tEvent)
   { 
     n = tEvent->Draw("y:x", "", "goff"); 
     g = new TGraph(n,tEvent->GetV2(),tEvent->GetV1()); 
-    g->SetTitle("; x (mm); y (mm)");
+    g->SetTitle("events; x (mm); y (mm)");
     g->Draw("ap");
     c.SaveAs(fout.Data());
     
     n = tEvent->Draw("y:z", "", "goff"); 
     g = new TGraph(n,tEvent->GetV2(),tEvent->GetV1()); 
-    g->SetTitle("; z (mm); y (mm)");
+    g->SetTitle("events; z (mm); y (mm)");
     g->Draw("ap");
     c.SaveAs(fout.Data());
     
-    print(c, fout, tEvent, "t", "", "", "; t (ns)");
+    print(c, fout, tEvent, "t", "", "", "events; t (ns)");
     print(c, fout, tEvent, "Enu", "", "", "neutrino; E (MeV)");
     print(c, fout, tEvent, "pxnu", "", "", "neutrino; px (MeV)");
     print(c, fout, tEvent, "pynu", "", "", "neutrino; py (MeV)");
@@ -225,12 +253,12 @@ int main(int argc, char* argv[])
     print(c, fout, tEvent, "pxnureco", EnuRecoOK, "", "neutrino; pxreco (MeV)");
     print(c, fout, tEvent, "pynureco", EnuRecoOK, "", "neutrino; pyreco (MeV)");
     print(c, fout, tEvent, "pznureco", EnuRecoOK, "", "neutrino; pzreco (MeV)");
-    print(c, fout, tEvent, "particles.primary", "", "", "; primary");
+    print(c, fout, tEvent, "particles.primary>>htemp(2,-0.5,1.5)", "", "", "particles; primary");
     
     tEvent->Draw("Enureco:Enu>>h2(100,0,10000,100,0,10000)", EnuRecoOK, "colz"); 
     h2 = (TH2D*) gROOT->FindObject("h2");
     h2->SetStats(false);
-    h2->SetTitle("; E (MeV); Ereco (MeV)");
+    h2->SetTitle("neutrino; E (MeV); Ereco (MeV)");
     h2->Draw("colz");
     c.SaveAs(fout.Data());
     
@@ -238,7 +266,7 @@ int main(int argc, char* argv[])
     tEvent->Draw("particles.pdg>>hpdg(8000,-4000,4000)", "", ""); 
     h1 = (TH1D*) gROOT->FindObject("hpdg");
     h1->SetStats(false);
-    h1->SetTitle("; pdg");
+    h1->SetTitle("particles; pdg");
     h1->Draw();
     c.SaveAs(fout.Data());
     
@@ -250,65 +278,74 @@ int main(int argc, char* argv[])
     c.SaveAs(fout.Data());
     c.SetLogy(false);
     
-    print(c, fout, tEvent, "particles.tr.ret_ln==0&&particles.tr.ret_cr==0", PrimaryPart, "", "primaries; recoOK==1");    
+    print(c, fout, tEvent, "(particles.tr.ret_ln==0&&particles.tr.ret_cr==0)>>htemp(2,-0.5,1.5)", PrimaryPart, "", "primaries; recoOK==1");    
     
-    tEvent->Draw("particles.charge_reco:particles.charge", PrimaryPart && PartTrackRecoOK, "colztext"); 
+    tEvent->Draw("particles.charge_reco:particles.charge>>htemp(3,-1.5,1.5,3,-1.5,1.5)", PrimaryPart && PartTrackRecoOK, "colztext"); 
     h2 = (TH2D*) gROOT->FindObject("htemp");
     h2->SetStats(false);
-    h2->SetTitle("primaries; true charge; reco charge");
+    h2->SetTitle("reco primaries; true charge; reco charge");
     h2->Draw("colztext");
     c.SaveAs(fout.Data());
     
-    tEvent->Draw("particles.pxreco:particles.pxtrue", PrimaryPart && PartTrackRecoOK, "colz"); 
+    tEvent->Draw("particles.pxreco:particles.pxtrue>>htemp(200,-2000,2000,200,-2000,2000)", PrimaryPart && PartTrackRecoOK, "colz"); 
     h2 = (TH2D*) gROOT->FindObject("htemp");
     h2->SetStats(false);
-    h2->SetTitle("primaries; true px (MeV); reco px (MeV)");
+    h2->SetTitle("reco primaries; true px (MeV); reco px (MeV)");
     h2->Draw("colz");
+    c.SaveAs(fout.Data());
     
-    tEvent->Draw("particles.pyreco:particles.pytrue", PrimaryPart && PartTrackRecoOK, "colz"); 
+    tEvent->Draw("particles.pyreco:particles.pytrue>>htemp(200,-2000,2000,200,-2000,2000)", PrimaryPart && PartTrackRecoOK, "colz"); 
     h2 = (TH2D*) gROOT->FindObject("htemp");
     h2->SetStats(false);
-    h2->SetTitle("primaries; true py (MeV); reco py (MeV)");
+    h2->SetTitle("reco primaries; true py (MeV); reco py (MeV)");
     h2->Draw("colz");
+    c.SaveAs(fout.Data());
     
-    tEvent->Draw("particles.pzreco:particles.pztrue", PrimaryPart && PartTrackRecoOK, "colz"); 
+    tEvent->Draw("particles.pzreco:particles.pztrue>>htemp(200,-1000,15000,200,-1000,15000)", PrimaryPart && PartTrackRecoOK, "colz"); 
     h2 = (TH2D*) gROOT->FindObject("htemp");
     h2->SetStats(false);
-    h2->SetTitle("primaries; true pz (MeV); reco pz (MeV)");
+    h2->SetTitle("reco primaries; true pz (MeV); reco pz (MeV)");
     h2->Draw("colz");
+    c.SaveAs(fout.Data());
     
-    tEvent->Draw("particles.Ereco:particles.Etrue", PrimaryPart && PartTrackRecoOK, "colz"); 
+    tEvent->Draw("particles.Ereco:particles.Etrue>>htemp(200,0,15000,200,0,15000)", PrimaryPart && PartTrackRecoOK, "colz"); 
     h2 = (TH2D*) gROOT->FindObject("htemp");
     h2->SetStats(false);
-    h2->SetTitle("primaries; true E (MeV); reco E (MeV)");
+    h2->SetTitle("reco primaries; true E (MeV); reco E (MeV)");
     h2->Draw("colz");
+    c.SaveAs(fout.Data());
     
-    tEvent->Draw("particles.xreco:particles.xtrue", PrimaryPart && PartTrackRecoOK, "colz"); 
+    tEvent->Draw("particles.xreco:particles.xtrue>>htemp(200,-2000,2000,200,-2000,2000)", PrimaryPart && PartTrackRecoOK, "colz"); 
     h2 = (TH2D*) gROOT->FindObject("htemp");
     h2->SetStats(false);
     h2->SetTitle("primaries; true x (mm); reco x (mm)");
     h2->Draw("colz");
+    c.SaveAs(fout.Data());
     
-    tEvent->Draw("particles.yreco:particles.ytrue", PrimaryPart && PartTrackRecoOK, "colz"); 
+    tEvent->Draw("particles.yreco:particles.ytrue>>htemp(200,-4500,-200,200,-4500,-200)", PrimaryPart && PartTrackRecoOK, "colz"); 
     h2 = (TH2D*) gROOT->FindObject("htemp");
     h2->SetStats(false);
     h2->SetTitle("primaries; true y (MeV); reco y (mm)");
     h2->Draw("colz");
+    c.SaveAs(fout.Data());
     
-    tEvent->Draw("particles.zreco:particles.ztrue", PrimaryPart && PartTrackRecoOK, "colz"); 
+    tEvent->Draw("particles.zreco:particles.ztrue>>htemp(200,22000,26000,200,22000,26000)", PrimaryPart && PartTrackRecoOK, "colz"); 
     h2 = (TH2D*) gROOT->FindObject("htemp");
     h2->SetStats(false);
     h2->SetTitle("primaries; true z (mm); reco z (mm)");
     h2->Draw("colz");
+    c.SaveAs(fout.Data());
     
     tEvent->Draw("particles.treco:particles.ttrue", PrimaryPart && PartTrackRecoOK, "colz"); 
     h2 = (TH2D*) gROOT->FindObject("htemp");
     h2->SetStats(false);
     h2->SetTitle("primaries; true t (ns); reco t (ns)");
     h2->Draw("colz");
-    
+    c.SaveAs(fout.Data());   
     
   }
+  
+  std::cout << " done" << std::endl;
   
   c.Clear();
   c.SaveAs(TString::Format("%s)",fout.Data()).Data());
