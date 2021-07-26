f650f31432ed455a4201a7021c9f2b1baa432b83
Gabriele Sirri <gabriele.sirri@bo.infn.it>
2021-07-26 09:50:47 +0200

    configure line-endings as Linux and renormalize
    Configure a .gitattributes file to manage how Git reads line endings in a specific repository.
    This prevents issues when located on shared filesystems (like WSL) that can be accessed by different OS,
    
    normalize the line endings:
    $ git add --renormalize .

--- clang-format-all.sh
+++ clang-format-all.sh
@@ -1,1 +1,6 @@
+# clang-format
 find ./src ./include -type f -not -name '*.cmd' -and -name '*.c*' -or -name '*.h*' | xargs clang-format -i --style=file
+
+# refresh line endings
+git add --renormalize .
+
