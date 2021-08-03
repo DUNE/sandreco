fa3cf3b110a516b791598e5a55742b80356efc34
Francesco Poppi <fpoppi@neutrino-01.novalocal>
2021-08-03 13:55:36 +0000

    Tolti conflitti

--- clang-format-all.sh
+++ clang-format-all.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-find ./src ./include -type f -not -name '*.cmd' -and -name '*.c*' -or -name '*.h*' | xargs clang-format -i --style=file
-
 # clang-format
 find ./src ./include -type f -not -name '*.cmd' -and -name '*.c*' -or -name '*.h*' | xargs clang-format -i --style=file
 
