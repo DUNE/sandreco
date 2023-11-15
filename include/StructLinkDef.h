435c8e2b4810f37d44602928060038c4f1333fb7
mt82 <matteo.tenti@gmail.com>
2023-11-15 12:34:26 +0000

    dictionaries added

--- include/StructLinkDef.h
+++ include/StructLinkDef.h
@@ -1,23 +1,23 @@
 #ifdef __CINT__
-//#include "struct.h"
+#include "struct.h"
 
 //#pragma link C++ class std::map < int, std::vector < double>> + ;
 //#pragma link C++ class std::map < int, std::vector < int>> + ;
 //#pragma link C++ class std::map < int, double> + ;
 //#pragma link C++ class std::vector < pe> + ;
 //#pragma link C++ class std::vector < dg_ps> + ;
-//#pragma link C++ class std::vector < dg_cell> + ;
+#pragma link C++ class std::vector < dg_cell> + ;
 //#pragma link C++ class std::map < std::string, std::vector < hit>> + ;
-//#pragma link C++ class std::vector < dg_tube> + ;
-//#pragma link C++ class std::vector < track> + ;
-//#pragma link C++ class std::vector < cluster> + ;
+#pragma link C++ class std::vector < dg_tube> + ;
+#pragma link C++ class std::vector < track> + ;
+#pragma link C++ class std::vector < cluster> + ;
 //#pragma link C++ class std::vector < particle> + ;
 //#pragma link C++ class pe + ;
 //#pragma link C++ class dg_ps + ;
-//#pragma link C++ class dg_tube + ;
-//#pragma link C++ class dg_cell + ;
-//#pragma link C++ class cluster + ;
-//#pragma link C++ class track + ;
+#pragma link C++ class dg_tube + ;
+#pragma link C++ class dg_cell + ;
+#pragma link C++ class cluster + ;
+#pragma link C++ class track + ;
 //#pragma link C++ class particle + ;
 #pragma link C++ class event + ;
 #endif
