caf543556bc3fec2eaf8aa19d9e7bc8fffb76f45
leadinoto <lea.dinoto@ge.infn.it>
2024-12-07 14:36:33 +0100

    For particles in tEvent

--- include/StructLinkDef.h
+++ include/StructLinkDef.h
@@ -20,6 +20,6 @@
 #pragma link C++ class reco_cell + ;
 #pragma link C++ class cluster + ;
 #pragma link C++ class track + ;
-//#pragma link C++ class particle + ;
+#pragma link C++ class particle + ;
 #pragma link C++ class event + ;
 #endif
